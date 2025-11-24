4ab569019e194e4228ba436991b50416e3dff570
Noobinvester <guizarbrandon@gmail.com>
2025-11-23 18:48:00 -0800

    11 13 25

---
+++ main.py
@@ -0,0 +1,24 @@
+from setting import *
+from level import Level
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("jumping man")
+
+        self.current_stage = Level()
+
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit
+
+            self.current_stage.run()
+
+            pygame.display.update()
+if __name__ == '__main__':
+    game = Game()
+    game.run()
