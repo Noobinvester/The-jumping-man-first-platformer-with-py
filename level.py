84771b7a985750010ceaa1d3a588c14ab449a462
Noobinvester <guizarbrandon@gmail.com>
2025-11-25 20:11:09 -0800

    first game practive update

--- level.py
+++
@@ -1,8 +0,0 @@
-from setting import *
-
-class Level:
-    def __init__(self):
-        self.display_surface = pygame.display.get_surface()
-
-    def run(self):
-        self.display_surface.fill('white')
--- main.py
+++ main.py
@@ -1,24 +1,60 @@
-from setting import *
-from level import Level
-class Game:
-    def __init__(self):
-        pygame.init()
-        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        pygame.display.set_caption("jumping man")
+import os 
+import random
+import math
+import pygame
+from os import listdir
+from os.path import isfile, join
+pygame.init()
 
-        self.current_stage = Level()
+pygame.display.set_caption("Jumping man")
+
+#BG_COLOR = (255, 255, 255)
+WIDTH = 1000
+HEIGHT = 800
+FPS = 60
+PLAYER_VEL= 5
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))  # Right - one tuple argument
 
 
-    def run(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
 
-            self.current_stage.run()
+    tiles = []
 
-            pygame.display.update()
-if __name__ == '__main__':
-    game = Game()
-    game.run()
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+
+    
+    return tiles, image
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    pygame.display.update()
+
+def main(window):
+    clock = pygame.time.Clock()
+    background, bg_image = get_background('Blue.png')
+
+    run = True
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type ==  pygame.QUIT:
+                run = False
+                break
+        
+        draw(window, background, bg_image)
+    pygame.quit()
+    quit()
+
+
+
+if __name__ == "__main__":
+    main(window)
